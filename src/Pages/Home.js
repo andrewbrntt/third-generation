9f0da36212a23ec09c8bb4a467922a5ad98fd0df
Andrew <andrewbrntt@gmail.com>
2022-02-01 17:40:16 -0700

    Gallery Refactor done. It no longer relies on the name of the image for needed information. It uses metadata added by the CDN.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,8 +34,7 @@ const Home = () => {
   }
 
   useEffect(() => {
-// TODO: The file structure needs to be gallery type => individual project => device size eg: basement => project-1 => desktop
-    const galleryImageGroup = getImageGroup(GLOBAL_DEFS.IMAGE_GROUPS.PROJECT_1)
+    const galleryImageGroup = getImageGroup(GLOBAL_DEFS.GALLERY_NAMES.BASEMENT)
 
     const aboutHero = getStockArtImage(GLOBAL_DEFS.PAGE_HEROS.ABOUT_US)
     const roofingCard = getStockArtImage(GLOBAL_DEFS.IMAGE_CARDS.ROOFING_CARD)
